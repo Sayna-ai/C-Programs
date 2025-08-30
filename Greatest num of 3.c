4173829425950e76e6f557c1b966c7f7abe704f1
Sayna-ai <bhramersayna@gmail.com>
2025-08-31 00:10:34 +0530

    Add files via upload

---
+++ Greatest num of 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int n1,n2,n3;
+  printf("Enter three values\n");
+  scanf("%d %d %d\n",&n1,&n2,&n3);
+  printf("%d %d %d\n",n1,n2,n3);
+  if(n1>n2 && n1>n2)
+  {
+    printf("Greatest num of three:%d",n1);
+  }
+  else if(n2>n1 && n2>n3)
+  {
+    printf("Greatest num of three:%d",n2);
+  }
+  else
+  {
+    printf("Greatest num of three:%d",n3);
+  }
+  return 0;
+}
