c46a0d76b0ebbd2a69eed988d11141526dc97173
Sayna-ai <bhramersayna@gmail.com>
2025-08-31 00:08:50 +0530

    Add files via upload

---
+++ leapYear.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Enter year:");
+    scanf("%d",&year);
+    if(year%4==0 || year%400==0)
+    {
+        printf("%d Is a leap year",year);
+    }
+    else
+    {
+        printf("%d Is not a leap year",year);
+    }
+    return 0;
+}
